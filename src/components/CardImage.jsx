c8363a3415b817db626e4ac4cc17799a24226cc1
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-28 10:58:23 -0600

    image component html & styling

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -1,7 +1,16 @@
 import "../styles/CardImage.css";
+import BurakImage from "../assets/Burak_Ephesus.jpg";
 
 function CardImage() {
-  return <div className="card--image"></div>;
+  return (
+    <div className="card--image_container">
+      <img
+        src={BurakImage}
+        alt="Burak at Ephesus, Turkey"
+        className="card--image"
+      />
+    </div>
+  );
 }
 
 export default CardImage;
