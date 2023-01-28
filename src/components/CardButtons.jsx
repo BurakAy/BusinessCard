a91d1b56a627826cbe1dee784c138ddad21ac222
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-28 12:43:47 -0600

    buttons component and styling; added fontawesome packages

--- src/components/CardButtons.jsx
+++ src/components/CardButtons.jsx
@@ -1,5 +1,24 @@
+import "../styles/CardButtons.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 const CardButtons = () => {
-  return <div className="card--buttons"></div>;
+  const envelopeIcon = <FontAwesomeIcon icon={faEnvelope} />;
+  const linkedinIcon = <FontAwesomeIcon icon={faLinkedin} />;
+
+  return (
+    <div className="card--buttons_container">
+      <button className="card--buttons_email">
+        {envelopeIcon}
+        Email
+      </button>
+      <button className="card--buttons_linkedin">
+        {linkedinIcon}
+        LinkedIn
+      </button>
+    </div>
+  );
 };
 
 export default CardButtons;
