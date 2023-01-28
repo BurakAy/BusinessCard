66a8f28c6099dee2ea1cb3c240d301d31090df42
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-28 12:41:42 -0600

    title component and styling

--- src/components/CardTitle.jsx
+++ src/components/CardTitle.jsx
@@ -1,5 +1,13 @@
+import "../styles/CardTitle.css";
+
 const CardTitle = () => {
-  return <div className="card--title"></div>;
+  return (
+    <div className="card--title_container">
+      <h1>Burak Aydemir</h1>
+      <h3>Frontend Engineer</h3>
+      <a href="https://burakaydemir.com">burakaydemir.com</a>
+    </div>
+  );
 };
 
 export default CardTitle;
